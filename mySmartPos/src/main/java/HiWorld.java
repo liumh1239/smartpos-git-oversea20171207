14c4ac39853bebfabae925a18fd4ffd181baff9c
liumh <suyuan1239@163.com>
2021-02-17 15:55:02 +0800

    ni8

--- mySmartPos/src/main/java/HiWorld.java
+++ mySmartPos/src/main/java/HiWorld.java
@@ -2,7 +2,7 @@ public class HiWorld {
     public static void main(String[] args) {
         System.out.println("nihao woshi liu");
         System.out.println("nihao woshi liu");
-        System.out.println("nihao woshi l");
+        System.out.println("niao woshi l");
         System.out.println("nihao woshi lu");
     }
 }
